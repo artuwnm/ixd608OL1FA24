9d361a989a5f3178acb09aa6c00222a2d047ffa0
sainanb <bisainan@msn.com>
2024-11-20 14:31:20 +0800

    W11 work in progress

--- bi.sainan/ArchiTaste/cart_actions.php
+++ bi.sainan/ArchiTaste/cart_actions.php
@@ -1,39 +1,12 @@
  <?php 
 
- include_once "lib/php/functions.php";
-
- function array_find($array,$fn) {
- 	foreach($array as $o) if($fn($o)) return $o;
- 	return false;
- }
-
- function getCart() {
- 	return isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
- }
-
- function addToCart($id,$amount,$options) {
- 	//$_SESSION['cart'] = [];
-	
- 	$cart = getCart();
-
- 	$p = array_find($cart,function($o) use($id) {return $o->id==$id;});
-
- 	if($p) {
- 		$p->amount += $amount;
- 	} else {
-	 	$_SESSION['cart'][] = (object)[
-	 		"id"=>$id,
-	 		"amount"=>$amount,
-	 		"options"=>$options
-		];
-	}
-}
+include_once "lib/php/functions.php";
 
 switch($_GET['action']) {
 		case 'add-to-cart':
 			$product = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_POST['product-id'])[0];
 			addToCart($_POST['product-id'],$_POST['product-amount'],$_POST['product-options']);	
-			//header("location:{$_SERVER['PHP_SELF']}? id = $id");
+			header("location:addedtocart.php? id = {$_POST['product-id']}");
 			break;
 			
 		case 'update-cart-item':
@@ -42,6 +15,10 @@ switch($_GET['action']) {
 		
 		case 'delete':
 			//header("location:{$_SERVER['PHP_SELF']}");
+			break;
+		
+		case 'reset-cart':
+			resetCart();
 			break;
 
 		default: 
