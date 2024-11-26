77f358c75a96d777e29376a8dd964da289a2c282
sainanb <bisainan@msn.com>
2024-11-26 10:07:28 +0800

    w12 progress

--- bi.sainan/ArchiTaste/data/api.php
+++ bi.sainan/ArchiTaste/data/api.php
@@ -12,19 +12,21 @@ switch($data->type) {
 	case "products_all":
 		$output['result'] = makeQuery(makeConn(),"SELECT *
 	        FROM `products`
-	        ORDER BY `date_create` DESC
+	        ORDER BY `custom_id` ASC
 	        LIMIT 15");
 		break;
 
 	case "products_search":
 		$output['result'] = makeQuery(makeConn(),"SELECT *
 	        FROM `products`
-	        WHERE `name` LIKE '%$data->search%'
-	        ORDER BY `date_create` DESC
+	        WHERE `name` LIKE '%$data->search%' OR
+	        WHERE `category` LIKE '%$data->search%' OR
+	        WHERE `description` LIKE '%$data->search%' OR
+	        ORDER BY `custom_id` ASC
 	        LIMIT 15");
 		break;
 
-	default: $output['error'] = "no valid type";
+	default: $output['error'] = "No Valid Type";
 }
 
 echo json_encode($output,JSON_NUMERIC_CHECK|JSON_UNESCAPED_UNICODE);
