e6a57e030bc55c9ca7862b8fa2aad8809ea1878b
Vicky Jia <vickyjia@vickys-laptop.lan>
2024-11-26 22:49:11 -0800

    module 12

--- jia.jia/product_list.php
+++ jia.jia/product_list.php
@@ -9,27 +9,64 @@ include_once "parts/templates.php";
 	<title>Product List</title>
 
 	<?php include "parts/meta.php"; ?>
+
+	<script src="lib/js/functions.js"></script>
+	<script src="js/templates.js"></script>
+	<script src="js/product_list.js"></script>
+
 </head>
 <body>
 	<?php include "parts/navbar.php"; ?>
 
 	<div class="container">
 			<h2>Product List</h2>
-				<?php
 
-	            $result = makeQuery(
-	            	makeConn(),
-	            	"
-	            	SELECT *
-	            	FROM `products`
-	            	ORDER BY `date_create` ASC
-	            	LIMIT 15
-	            	"
-	            );
-            	
-	            echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
+			<div class="form-control">
+				<form class="hotdog light" id="product-search">
+					<input type="search" placeholder="Search Product">
+				</form>
+			</div>
+			<div class="form-control">
+				<div class="card soft">
+					<div class="display-flex">
+						<div class="flex-stretch display-flex">
+							<div class="grid gap">
+				
+							<div class="flex-none">
+								<button data-filter="category" data-value="" type="button" class="form-button">All</button>
+							</div>
+							<div class="flex-none">
+								<button data-filter="category" data-value="living" type="button" class="form-button">Living</button>
+							</div>
+							<div class="flex-none">
+								<button data-filter="category" data-value="bedroom" type="button" class="form-button">Bedroom</button>
+							</div>
+							<div class="flex-none">
+								<button data-filter="category" data-value="dining" type="button" class="form-button">Dining</button>
+							</div>
+							<div class="flex-none">
+								<button data-filter="category" data-value="kitchen" type="button" class="form-button">Kitchen</button>
+							</div>
+							</div>
+						</div>
+				
+						<div class="flex-none">
+							<div class="form-select">
+								<select class="js-sort">
+									<option value="1">Newest</option>
+									<option value="2">Oldest</option>
+									<option value="3">Least Expensive</option>
+									<option value="4">Most Expensive</option>
+									<option value="5">Most Popular</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
-	?>
+			<div class='productlist grid gap'></div>
+			
 	</div>
 </body>
 </html>
